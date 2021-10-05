4e7d2da3bc9db050cd7850d46036fe8908fd95bb
YeShuai <YeShuai@pf.com>
2021-10-05 15:40:22 +0800

    first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,7 +3,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello mayun");
-
+        System.out.println("hot-fix did");
     }
-
 }
